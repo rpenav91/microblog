47873891abc931291231a8c6ad0eb798f1b36535
René Peña <rpena0391@gmail.com>
2022-04-08 17:09:57 -0600

    Events

--- query/index.js
+++ query/index.js
@@ -9,14 +9,23 @@ app.use(cors());
 const posts = {};
 
 app.get('/posts', (req, res) => {
-
+    res.send(posts)
 })
 
-app.get('/events', (req, res) => {
+app.post('/events', (req, res) => {
     const { type, data } = req.body;
     if (type == "PostCreated") {
-
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] }
     }
+
+    if (type == "CommentCreated") {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    res.send({})
 
 })
 
